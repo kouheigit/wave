36ad1766c15419e8c3236872d83af46387ddb31a
ookawa0602 <forefront.ookawa2@gmail.com>
2020-06-30 00:01:34 +0900

    2020.6.30

---
+++ mobile/index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+   <meta charset="utf-8">
+   <link href="css/mobilemain.css" rel="stylesheet"> 
+ </head>
+  <body>
+  <header>
+    <h1>弦巻・千歳船橋でクリーニング店を選ぶなら</h1>
+    <h2>クリーニングWAVE</h2>
+  </header>
+ </body>
+</html>
+

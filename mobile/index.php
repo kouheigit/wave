328964a64d24e27c0b6569fa28688ad4b2eb3ee1
ookawa0602 <forefront.ookawa2@gmail.com>
2020-07-01 16:16:07 +0900

    test

--- mobile/index.php
+++ mobile/index.php
@@ -1,7 +1,7 @@
 <html>
  <head>
    <meta charset="utf-8">
-   <link href="css/mobilemain.css" rel="stylesheet"> 
+   <link href="css/mobilemain.css" rel="stylesheet">
  </head>
   <body>
   <header>

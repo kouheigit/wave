b151ecfe18f08748b68e158426ffa8cc7d863297
ookawa0602 <forefront.ookawa2@gmail.com>
2020-07-01 23:57:11 +0900

    drawer branch

--- mobile/index.php
+++ mobile/index.php
@@ -2,6 +2,7 @@
  <head>
    <meta charset="utf-8">
    <link href="css/mobilemain.css" rel="stylesheet">
+   <link href="css/drawer.css" rel="stylesheet">
   <!--drawer-->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/drawer/3.1.0/css/drawer.min.css">
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -27,7 +28,7 @@
      <span class="sr-only">toggle navigation</span>
      <span class="drawer-hamburger-icon"></span>
    </button>
-   <nav class="drawer-nav" role="navigation">
+    <nav class="drawer-nav" role="navigation">
     <ul class="drawer-menu">
       <li><a class="drawer-brand" href="#">Brand</a></li>
       <li><a class="drawer-menu-item" href="#">Nav1</a></li>
